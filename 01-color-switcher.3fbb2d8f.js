47d41fd013dffc565609482faf1972963bb86621
IamVovaMoroz <113520901+IamVovaMoroz@users.noreply.github.com>
2023-03-19 15:21:02 +0000

    Deploying to gh-pages from @ IamVovaMoroz/parcel-project-template@82d688fcf5028d5ae658f9ebb02addeae2c17bbd 🚀

---
+++ 01-color-switcher.3fbb2d8f.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body");let r=null;function o(){return`#${Math.floor(16777215*Math.random()).toString(16)}`}t.addEventListener("click",(()=>{r=setInterval((()=>{n.style.background=o()}),1e3),t.disabled=!0})),e.addEventListener("click",(()=>{clearInterval(r),t.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.3fbb2d8f.js.map
