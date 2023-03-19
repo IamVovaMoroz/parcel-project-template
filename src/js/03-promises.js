93be1d69b9aba3fae154a14d82119acf5155aef8
IamVovaMoroz <113520901+IamVovaMoroz@users.noreply.github.com>
2023-03-19 15:15:23 +0300

    ok

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,78 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+// let delayInputRef = document.querySelector('input[name="delay"]');
+// console.log((delayInputRef.value = 2));
+// let stepInputRef = document.querySelector('input[name="step"]');
+// console.log(stepInputRef);
+// let amountInputRef = document.querySelector('input[name="amount"]');
+// console.log(amountInputRef);
+// const startBtn = document.querySelector('button[type="submit"]');
+// console.log(startBtn);
+
+// // startBtn.addEventListener('submit');
+// function createPromise (position, delay) {
+//   const shouldResolve = Math.random() > 0.3;
+//   if (shouldResolve) {
+
+//     // Fulfill
+//   } else {
+//     // Reject
+//   }
+// }
+
+// function run (horse) {
+//   return new Promise(resolve => {
+//     const time = 2000;
+
+//     setInterval(() => {
+//       resolve(time + 2000);
+//     }, time);
+//   });
+// }
+
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('.form');
+let delayInput = null;
+let stepInput = null;
+let amountInput = null;
+// const firstDelayMs = document.querySelector('[name="delay"]');
+// const delayStepMs = document.querySelector('[name="step"]');
+// const amount = document.querySelector('[name="amount"]');
+
+function createPromise (position, delay) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+form.addEventListener('submit', handleSubmit);
+function handleSubmit (event) {
+  event.preventDefault();
+  const {
+    elements: { delay, step, amount },
+  } = event.currentTarget;
+  let delayInput = Number(delay.value);
+  let stepInput = Number(step.value);
+  let amountInput = Number(amount.value);
+
+  for (let i = 1; i <= amountInput; i += 1) {
+    createPromise(i, delayInput)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+    delayInput += stepInput;
   }
+  event.currentTarget.reset();
 }
