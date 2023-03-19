b61731efd6838f841197ae90ffb073b51a9da93a
IamVovaMoroz <113520901+IamVovaMoroz@users.noreply.github.com>
2023-03-19 18:51:18 +0300

    ok

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -5,7 +5,8 @@ let delayInput = null;
 let stepInput = null;
 let amountInput = null;
 
-// Функция создаем Promise пропускает через себя да и нет
+// Функция создаем Promise выдаёт номер операциии задержку.
+// delay принимается каждый раз новый при новой операции, в заивсимости от заданого
 function createPromise (position, delay) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
@@ -15,36 +16,41 @@ function createPromise (position, delay) {
       } else {
         reject({ position, delay });
       }
-      // console.log(position);
-      // console.log(delay);
     }, delay);
   });
 }
+// После отправки формы запускаем функцию
 form.addEventListener('submit', formSubmit);
 function formSubmit (event) {
   event.preventDefault();
+  // Получили доступ к input (delay, step, amount) через event.currentTarget.elements
   const {
     elements: { delay, step, amount },
   } = event.currentTarget;
   let delayInput = Number(delay.value);
-  console.log(delayInput);
-  let stepInput = Number(step.value);
-  console.log(stepInput);
-  let amountInput = Number(amount.value);
-  console.log(amountInput);
 
+  let stepInput = Number(step.value);
+
+  let amountInput = Number(amount.value);
+
+  // Создаём цикл, который будет добавлять +1 пока не достигнит кол-ва указанного в строке amount
   for (let i = 1; i <= amountInput; i += 1) {
+    // Функция создаёт i(номер итерации amount), delayInput
     createPromise(i, delayInput)
+      // then получает позитивный (resolve) результат Promise номер операции и задержку
       .then(({ position, delay }) => {
+        // выводим через Notiflix полученные данные
         Notiflix.Notify.success(
           `✅ Fulfilled promise ${position} in ${delay}ms`
         );
       })
+      // then получает негативный (reject) результат Promise номер операции и задержку
       .catch(({ position, delay }) => {
         Notiflix.Notify.failure(
           `❌ Rejected promise ${position} in ${delay}ms`
         );
       });
+    // delayInput (первоначальная задержка) + stepInput(добавляем delay step на каждой итерации)
     delayInput += stepInput;
   }
   event.currentTarget.reset();
